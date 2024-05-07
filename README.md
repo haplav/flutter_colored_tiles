8e658d5c040c6fe1281991cb3f0871d191a9103f
Vaclav Hapla <vaclav.hapla@erdw.ethz.ch>
2024-05-07 15:32:49 +0200

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# FlutterColoredTiles
+
+A training Flutter project based on https://medium.com/flutter/keys-what-are-they-good-for-13cb51742e7d
---
+++ lib/main.dart
@@ -0,0 +1,63 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MaterialApp(home: PositionedTiles()));
+
+class PositionedTiles extends StatefulWidget {
+  const PositionedTiles();
+
+  @override
+  State<StatefulWidget> createState() => PositionedTilesState();
+}
+
+class PositionedTilesState extends State<PositionedTiles> {
+  List<Widget> tiles = [
+    ColorfulTile(),
+    ColorfulTile(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Row(children: tiles),
+      floatingActionButton: FloatingActionButton(
+        onPressed: swapTiles,
+        child: const Icon(Icons.sentiment_very_satisfied),
+      ),
+    );
+  }
+
+  swapTiles() {
+    setState(() {
+      tiles.insert(1, tiles.removeAt(0));
+    });
+  }
+}
+
+class ColorfulTile extends StatelessWidget {
+  final Color myColor;
+
+  ColorfulTile() : myColor = UniqueColorGenerator.getColor();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: myColor,
+      child: const Padding(padding: EdgeInsets.all(70.0)),
+    );
+  }
+}
+
+class UniqueColorGenerator {
+  static Random random = Random();
+
+  static Color getColor() {
+    return Color.fromARGB(
+      0xFF,
+      random.nextInt(0x100),
+      random.nextInt(0x100),
+      random.nextInt(0x100),
+    );
+  }
+}
