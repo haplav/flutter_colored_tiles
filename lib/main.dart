80a2c51ad690abd4d6889b7453f8e56cd9e6ffa8
Vaclav Hapla <vaclav.hapla@erdw.ethz.ch>
2024-05-17 15:38:58 +0200

    factor out MyApp

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,21 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-void main() => runApp(ChangeNotifierProvider(
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
       create: (context) => PositionedTilesState(3),
       child: const MaterialApp(
         home: PositionedTiles(),
       ),
-    ));
+    );
+  }
+}
 
 class PositionedTiles extends StatelessWidget {
   const PositionedTiles({super.key});
